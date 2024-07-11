4847e0d9dbec12d79cfc14073fe8a1424331b068
Beshoykhalil <peshoyfouad@gmail.com>
2024-07-10 21:21:17 -0400

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Batch 19 Is Great");
+            Thread.sleep(2000);
+        }
+
 
     }
 
